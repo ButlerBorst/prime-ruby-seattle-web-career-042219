7fd23dc96ed444aef2c247a97ed1463447d49307
Alex Borst <butler.borst@gmail.com>
2019-04-15 16:48:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
-def prime?(num)
-  false if num < 2
-  (2...num).each do |factor|
-    if (num % factor) <= 0
-      return false
-    end
+def prime(n)
+  unless n.is_a? Integer
+  puts "That's not an integer."
+  if n%2 !=0 && n!=2
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
   end
-  true 
 end
-
