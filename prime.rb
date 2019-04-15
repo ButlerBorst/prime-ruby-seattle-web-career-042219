685f1b30959dc8d277b0503c70384811319cc18c
Alex Borst <butler.borst@gmail.com>
2019-04-15 16:52:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
-def prime(num)
-  (2..num/2).none?{|i| num % i <= 0}
+def prime? (num)
+    if num <= 1
+        false
+    elsif num == 2
+        true
+    else 
+        (2..num/2).none? { |i| n % i == 0}
+    end
 end
